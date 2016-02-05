817563f2fd6c6d911beb1a9ff3f0a8a778451a6f
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-02-05 11:23:31 +0100

    Add Spanish translation

---
+++ lang/es.js
@@ -0,0 +1,6 @@
+CKEDITOR.plugins.setLang( 'html5audio', 'es', {
+    button: 'Insertar audio HTML5',
+    title: 'Audio HTML5',
+    infoLabel: 'Información del audio',
+    urlMissing: 'La URL del audio no puede estar vacia.'
+} );
