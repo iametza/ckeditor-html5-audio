255d21b6ca71a6dea729cfe9c00a545d5764a0eb
BB <bahriddin.abdiev@gmail.com>
2017-01-04 15:13:34 +0500

    Update uz.js

--- html5audio/lang/uz.js
+++ html5audio/lang/uz.js
@@ -1,8 +1,8 @@
 CKEDITOR.plugins.setLang( 'html5audio', 'uz', {
     button: 'HTML5 audio qo‘shing',
     title: 'HTML5 audio',
-    infoLabel: 'Audio ma'lumot',
-    urlMissing: 'Audio'ning URL manbasi topilmadi.',
+    infoLabel: 'Audio ma\'lumot',
+    urlMissing: 'Audio\'ning URL manbasi topilmadi.',
     audioProperties: 'Audio xususiyatlari',
     upload: 'Yuklash',
     btnUpload: 'Serverga jo‘natish',
